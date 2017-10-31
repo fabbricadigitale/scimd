a6cd688e13cd41fd48973e71ae50957e420b15ce
Leonardo Di Donato <leodidonato@gmail.com>
2017-10-31 13:04:45 +0100

    New: ListResponse struct

---
+++ api/messages/list.go
@@ -0,0 +1,10 @@
+package messages
+
+// ListResponse ...
+type ListResponse struct {
+	Schemas      []string   `json:"schemas"`
+	TotalResults int        `json:"totalResults"`
+	ItemsPerPage int        `json:"itemsPerPage"`
+	StartIndex   int        `json:"startIndex"`
+	Resources    []Resource `json:"Resources"`
+}
