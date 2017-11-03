7fc5ff77b3c4e927336e34a6dc7c520d359aae64
Leonardo Grasso <me@leonardograsso.com>
2017-11-03 18:32:20 +0100

    WIP: Unmarshal generic resource

--- api/messages/resource.go
+++ api/messages/resource.go
@@ -1,11 +1,112 @@
 package messages
 
 import (
+	"encoding/json"
+
+	"github.com/fabbricadigitale/scimd/schemas"
 	"github.com/fabbricadigitale/scimd/schemas/core"
 )
 
+// Common resource attributes
+type Common core.Common
+
+// A ScimError is a description of a SCIM error.
+type ScimError struct {
+	msg string // description of error
+}
+
+func (e *ScimError) Error() string { return e.msg }
+
 // Resource The data resource structure
 type Resource struct {
-	Common     core.Common
-	Attributes map[string]core.Complex
+	Common
+	Attributes map[string]core.Complex `json:"-"`
+}
+
+func unmarshalAttrs(schema *core.Schema, parts map[string]json.RawMessage) (*core.Complex, error) {
+	attrs := core.Complex{}
+	for _, aDef := range schema.Attributes {
+		if part, ok := parts[aDef.Name]; ok {
+			var value interface{}
+			if err := json.Unmarshal(part, &value); err != nil {
+				return nil, err
+			}
+			attrs[aDef.Name] = value
+		}
+	}
+
+	return &attrs, nil
+}
+
+func getSchema(schema string, allowedSchemas []string) *core.Schema {
+	repo := schemas.Repository()
+	for _, s := range allowedSchemas {
+		if s == schema {
+			return repo.GetSchema(s)
+		}
+	}
+
+	return nil
+}
+
+func (r *Resource) UnmarshalJSON(b []byte) error {
+
+	repo := schemas.Repository()
+
+	// Unmarshal common parts
+	if err := json.Unmarshal(b, &r.Common); err != nil {
+		return err
+	}
+
+	// Validate and get ResourceType
+	resourceType := repo.GetResourceType(r.Common.Meta.ResourceType)
+	if resourceType == nil {
+		return &ScimError{"Unsupported Resource Type"}
+	}
+
+	// Validate and get schema
+	baseSchema := getSchema(resourceType.Schema, r.Common.Schemas)
+	if baseSchema == nil {
+		return &ScimError{"Unsupported Schema"}
+	}
+
+	// Unmarshal othe parts
+	var parts map[string]json.RawMessage
+	if err := json.Unmarshal(b, &parts); err != nil {
+		return err
+	}
+
+	var err error
+	r.Attributes = make(map[string]core.Complex)
+
+	// Grab base schema attributes
+	var baseAttrs *core.Complex
+	baseAttrs, err = unmarshalAttrs(baseSchema, parts)
+	if err != nil {
+		return err
+	}
+	r.Attributes[baseSchema.ID] = *baseAttrs
+
+	// Grab extension schemas attributes
+	for _, schExt := range resourceType.SchemaExtensions {
+
+		if extRawMsg, ok := parts[schExt.Schema]; ok {
+
+			var extParts map[string]json.RawMessage
+			if err := json.Unmarshal(extRawMsg, &extParts); err != nil {
+				return err
+			}
+
+			if extSchema := getSchema(schExt.Schema, r.Common.Schemas); extSchema != nil {
+
+				var attrs *core.Complex
+				attrs, err = unmarshalAttrs(extSchema, extParts)
+				r.Attributes[extSchema.ID] = *attrs
+			}
+
+		}
+
+	}
+
+	return nil
 }
---
+++ api/messages/resource_test.go
@@ -0,0 +1,86 @@
+package messages
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+
+	"github.com/fabbricadigitale/scimd/schemas"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUnmarshalResource(t *testing.T) {
+
+	repo := schemas.Repository()
+	if err := repo.LoadResourceType("../../schemas/core/testdata/user.json"); err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	if err := repo.LoadSchema("../../schemas/core/testdata/user_schema.json"); err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	if err := repo.LoadSchema("../../schemas/core/testdata/enterprise_user_schema.json"); err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+
+	// Non-normative of SCIM user resource type [https://tools.ietf.org/html/rfc7643#section-8.2]
+	dat, err := ioutil.ReadFile("testdata/enterprise_user_resource.json")
+
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+
+	require.NotNil(t, dat)
+	require.Nil(t, err)
+
+	res := Resource{}
+	err = json.Unmarshal(dat, &res)
+
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+
+	equalities := []struct {
+		value interface{}
+		field interface{}
+	}{
+		{"2819c223-7f76-453a-919d-413861904646", res.ID},
+		{"2819c223-7f76-453a-919d-413861904646", res.Common.ID},
+		{[]string{"urn:ietf:params:scim:schemas:core:2.0:User", "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"}, res.Schemas},
+		{[]string{"urn:ietf:params:scim:schemas:core:2.0:User", "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"}, res.Common.Schemas},
+		{"https://example.com/v2/Users/2819c223-7f76-453a-919d-413861904646", res.Meta.Location},
+		{"https://example.com/v2/Users/2819c223-7f76-453a-919d-413861904646", res.Common.Meta.Location},
+	}
+
+	for _, row := range equalities {
+		assert.Equal(t, row.value, row.field)
+	}
+
+	baseAttr, baseAttrOk := res.Attributes["urn:ietf:params:scim:schemas:core:2.0:User"]
+	assert.Equal(t, true, baseAttrOk)
+
+	extAttr, extAttrOk := res.Attributes["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"]
+	assert.Equal(t, true, extAttrOk)
+
+	attrEqualities := []struct {
+		value interface{}
+		field interface{}
+	}{
+		{"bjensen@example.com", baseAttr["userName"]},
+		{"Babs Jensen", baseAttr["displayName"]},
+		{true, baseAttr["active"]},
+
+		{"701984", extAttr["employeeNumber"]},
+		{"4130", extAttr["costCenter"]},
+	}
+
+	for _, row := range attrEqualities {
+		assert.Equal(t, row.value, row.field)
+	}
+
+}
