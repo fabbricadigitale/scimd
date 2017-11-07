395c779116818a7cf4bbce86722170f24ccc04a9
Leonardo Grasso <me@leonardograsso.com>
2017-11-07 11:40:54 +0100

    New: PatchOp message

--- api/messages/patch.go
+++ api/messages/patch.go
@@ -1,1 +1,17 @@
 package messages
+
+const PATCH_OP_URN = "urn:ietf:params:scim:api:messages:2.0:PatchOp"
+
+type PatchOp struct {
+	Schemas    []string `json:"schemas"`
+	Operations []struct {
+		Op    string `json:"op"`
+		Path  string `json:"path,omitempty"`
+		Value []struct {
+			Display string `json:"display,omitempty"`
+			Ref     string `json:"$ref,omitempty"`
+			Value   string `json:"value"`
+			Type    string `json:"type,omitempty"`
+		} `json:"value,omitempty"`
+	} `json:"Operations"`
+}
