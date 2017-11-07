933a069caf96354977b96498316b6824c3b69b1b
Leonardo Grasso <me@leonardograsso.com>
2017-11-07 11:40:24 +0100

    New: Error message

--- api/messages/error.go
+++ api/messages/error.go
@@ -1,1 +1,10 @@
 package messages
+
+const ERROR_URN = "urn:ietf:params:scim:api:messages:2.0:Error"
+
+type Error struct {
+	Schemas  []string `json:"schemas"`
+	Status   string   `json:"status"`
+	ScimType string   `json:"scimType,omitempty"`
+	Detail   string   `json:"detail,omitempty"`
+}
