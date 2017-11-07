2b7071347c5df122c32eaaab13e2ee0ef7a9bba0
Leonardo Grasso <me@leonardograsso.com>
2017-11-07 11:40:41 +0100

    Update: List response

--- api/messages/list.go
+++ api/messages/list.go
@@ -1,10 +1,12 @@
 package messages
 
+const LIST_RESPONSE_URN = "urn:ietf:params:scim:api:messages:2.0:ListResponse"
+
 // ListResponse ...
 type ListResponse struct {
-	Schemas      []string `json:"schemas"`
-	TotalResults int      `json:"totalResults"`
-	ItemsPerPage int      `json:"itemsPerPage"`
-	StartIndex   int      `json:"startIndex"`
-	// Resources    []Resource `json:"Resources"`
+	Schemas      []string    `json:"schemas"`
+	TotalResults int         `json:"totalResults"`
+	ItemsPerPage int         `json:"itemsPerPage"`
+	StartIndex   int         `json:"startIndex"`
+	Resources    []*Resource `json:"Resources"`
 }
