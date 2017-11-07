b6692369a0e8ce4ad98e0806a5a2b3b75cd178bf
Leonardo Grasso <me@leonardograsso.com>
2017-11-07 11:41:23 +0100

    New: SearchRequest message

--- api/messages/search.go
+++ api/messages/search.go
@@ -1,1 +1,14 @@
 package messages
+
+const SEARCH_REQUEST_URN = "urn:ietf:params:scim:api:messages:2.0:SearchRequest"
+
+type SearchRequest struct {
+	Schemas            []string `json:"schemas"`
+	Attributes         []string `json:"attributes,omitempty"`
+	ExcludedAttributes []string `json:"excludedAttributes,omitempty"`
+	Filter             string   `json:"filter,omitempty"`
+	SortBy             string   `json:"sortBy,omitempty"`
+	SortOrder          string   `json:"sortOrder,omitempty"`
+	StartIndex         int      `json:"startIndex,omitempty"`
+	Count              int      `json:"count,omitempty"`
+}
