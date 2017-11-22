2aaf581580da1b8444697b3ac0b83de5cee9660d
alessandro <sandrone.lb@gmail.com>
2017-11-22 16:41:48 +0100

    Update: Add more Error type

--- api/messages/error.go
+++ api/messages/error.go
@@ -1,8 +1,10 @@
 package messages
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/fabbricadigitale/scimd/api"
 	"github.com/fabbricadigitale/scimd/schemas/core"
 )
 
@@ -15,18 +17,30 @@ type Error struct {
 	Detail   string   `json:"detail,omitempty"`
 }
 
+// ErrorWrapper wraps error in a scim Error struct
 func ErrorWrapper(e error) Error {
 
 	var scimError Error
 
 	switch e.(type) {
 	case *core.DataTypeError:
-
-		scimError.Schemas = append(scimError.Schemas, ErrorURN)
 		scimError.Status = string(http.StatusBadRequest)
-		scimError.Detail = e.Error()
 		scimError.ScimType = "invalidType"
+	case *json.UnmarshalTypeError:
+		scimError.Status = string(http.StatusBadRequest)
+		scimError.ScimType = "invalidType"
+	case *api.InvalidPathError:
+		scimError.Status = string(http.StatusBadRequest)
+		scimError.ScimType = "invalidPath"
+	case *api.InvalidFilterError:
+		scimError.Status = string(http.StatusBadRequest)
+		scimError.ScimType = "invalidFilter"
+	default:
+		scimError.Status = string(http.StatusInternalServerError)
 	}
+
+	scimError.Schemas = append(scimError.Schemas, ErrorURN)
+	scimError.Detail = e.Error()
 
 	return scimError
 }
--- api/search.go
+++ api/search.go
@@ -1,5 +1,7 @@
 package api
 
+import "fmt"
+
 // TODO: validation
 
 type Attributes struct {
@@ -29,4 +31,30 @@ type Search struct {
 	Filter `json:"filter,omitempty"`
 	Sorting
 	Pagination
+}
+
+// InvalidPathError is thrown when path attribute is invalid or malformed
+type InvalidPathError struct {
+	Path   string
+	Detail string
+}
+
+func (e *InvalidPathError) Error() string {
+	if len(e.Path) > 0 {
+		return fmt.Sprintf("Path [%s] is invalid: %s", e.Path, e.Detail)
+	}
+	return fmt.Sprintf("Path is invalid: %s", e.Detail)
+}
+
+// InvalidFilterError is thrown when the specified filter syntax is invalid or the specified attribute and filter comparison is not supported
+type InvalidFilterError struct {
+	Filter string
+	Detail string
+}
+
+func (e *InvalidFilterError) Error() string {
+	if len(e.Filter) > 0 {
+		return fmt.Sprintf("Filter [%s] is invalid: %s", e.Filter, e.Detail)
+	}
+	return fmt.Sprintf("Filter is invalid: %s", e.Detail)
 }
