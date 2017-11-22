d9b30f5f73ccd4ea987a98c61ac6d7f25cbfd87d
Samuele Salines <s.salines@fabbricadigitale.it>
2017-11-22 15:14:12 +0100

    Update: Handled panic and tag failing error

--- validation/endswith_test.go
+++ validation/endswith_test.go
@@ -1,27 +1,45 @@
 package validation
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	validator "gopkg.in/go-playground/validator.v9"
 )
 
 type TestEW struct {
-	Text string `validate:"endswith=r,required"`
+	Text    string `json:"text" validate:"endswith=r"`
+	Integer int    `json:"integer" validate:"endswith=r"`
 }
 
 func TestEndswith(t *testing.T) {
 	x := TestEW{}
 
+	fields := []string{"Text", "Integer"}
+	failtags := []string{"endswith", "endswith"}
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Panic recovered: ", r)
+		}
+	}()
+
 	// Ends with
 	x.Text = "bar"
 
 	errors := Validator.Struct(x)
-	require.NoError(t, errors, "err", nil)
+	require.NoError(t, errors)
 
 	// Doesn't ends with
 	x.Text = "bars"
 
 	errors = Validator.Struct(x)
 	require.Error(t, errors)
+
+	for e, err := range errors.(validator.ValidationErrors) {
+		require.Equal(t, "TestEW."+fields[e], err.Namespace())
+		require.Equal(t, fields[e], err.Field())
+		require.Equal(t, failtags[e], err.ActualTag())
+	}
 }
--- validation/startswith_test.go
+++ validation/startswith_test.go
@@ -1,27 +1,45 @@
 package validation
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	validator "gopkg.in/go-playground/validator.v9"
 )
 
 type TestSW struct {
-	Text string `validate:"startswith=b,required"`
+	Text string `validate:"startswith=b"`
+	// Integer int    `validate:"startswith=b"`
 }
 
 func TestStartswith(t *testing.T) {
 	x := TestSW{}
 
+	fields := []string{"Text", "Integer"}
+	failtags := []string{"startswith", "startswith"}
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Panic recovered: ", r)
+		}
+	}()
+
 	// Starts with
 	x.Text = "bar"
 
 	errors := Validator.Struct(x)
-	require.NoError(t, errors, "err", nil)
+	require.NoError(t, errors)
 
 	// Doesn't starts with
 	x.Text = "zar"
 
 	errors = Validator.Struct(x)
 	require.Error(t, errors)
+
+	for e, err := range errors.(validator.ValidationErrors) {
+		require.Equal(t, "TestSW."+fields[e], err.Namespace())
+		require.Equal(t, fields[e], err.Field())
+		require.Equal(t, failtags[e], err.ActualTag())
+	}
 }
