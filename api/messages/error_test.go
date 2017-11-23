0b923dd12d61042d142eeb58a1f4edcb64708aaa
a.priori <a.priori@fabbricadigitale.it>
2017-11-23 11:38:00 +0100

    New: Testing scim error wrapper

--- api/messages/error_test.go
+++ api/messages/error_test.go
@@ -1,1 +1,41 @@
 package messages
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/fabbricadigitale/scimd/schemas/core"
+	"github.com/stretchr/testify/require"
+)
+
+func TestErrorWrapper(t *testing.T) {
+
+	// Unexpected Data Type
+	var s = "randomvalue"
+	_, err := core.NewDataType(s)
+
+	require.Equal(t, Error{
+		Schemas:  append([]string{}, ErrorURN),
+		Status:   string(http.StatusBadRequest),
+		ScimType: "invalidValue",
+		Detail:   err.Error(),
+	}, ErrorWrapper(err))
+
+	// Unmarshal type error
+	type Prova struct {
+		Num string `json:"num"`
+	}
+	p := Prova{}
+	byt := []byte(`{"num": 13}`)
+
+	err = json.Unmarshal(byt, &p)
+
+	require.Equal(t, Error{
+		Schemas:  append([]string{}, ErrorURN),
+		Status:   string(http.StatusBadRequest),
+		ScimType: "invalidValue",
+		Detail:   err.Error(),
+	}, ErrorWrapper(err))
+
+}
