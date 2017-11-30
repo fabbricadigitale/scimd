3efba794ba9b92a2c50193aa092667b32b90ae93
skryja <alessandro.priori90@gmail.com>
2017-11-30 16:52:17 +0100

    Update: Added validation tags

--- api/messages/search.go
+++ api/messages/search.go
@@ -4,9 +4,11 @@ import (
 	"github.com/fabbricadigitale/scimd/api"
 )
 
+// SearchRequestURI is the URI identifies the query request
 const SearchRequestURI = "urn:ietf:params:scim:api:messages:2.0:SearchRequest"
 
+// SearchRequest represents a resource query as per https://tools.ietf.org/html/rfc7644#section-3.4.3
 type SearchRequest struct {
-	Schemas []string `json:"schemas"`
+	Schemas []string `json:"schemas" validate:"eq=1,dive,eq=urn:ietf:params:scim:api:messages:2.0:SearchRequest"`
 	api.Search
 }
