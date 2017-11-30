a7e8a9afae6a85e8aec46b94a74564b2424136ad
Samuele Salines <samuele.saliness@gmail.com>
2017-11-30 12:42:38 +0100

    Update: Urn validator implementation (refs #33)

--- validation/urn.go
+++ validation/urn.go
@@ -1,11 +1,24 @@
 package validation
 
 import (
+	"fmt"
+	"reflect"
+
+	"github.com/fabbricadigitale/scimd/api/attr"
+	"github.com/fabbricadigitale/scimd/schemas"
 	validator "gopkg.in/go-playground/validator.v9"
 )
 
 var urn = func(fl validator.FieldLevel) bool {
-	// (todo)> implement
+	field := fl.Field()
 
-	return true
+	switch field.Kind() {
+	case reflect.String:
+		str := field.String()
+		if attr.Parse(str).Valid() != true {
+			panic(fmt.Sprintf("Invalid URN composition: %+v", field.Interface()))
+		}
+		return schemas.URIRegexp.MatchString(str)
+	}
+	panic(fmt.Sprintf("Bad field type %T", field.Interface()))
 }
