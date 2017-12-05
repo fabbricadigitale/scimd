3120f24064bd272e8c35928f9928796014c65d50
skryja <alessandro.priori90@gmail.com>
2017-12-05 16:49:20 +0100

    Update: Check struct kind of isset tag's parent

--- validation/isset.go
+++ validation/isset.go
@@ -1,10 +1,18 @@
 package validation
 
 import (
+	"fmt"
+	"reflect"
+
 	validator "gopkg.in/go-playground/validator.v9"
 )
 
 var depsOn = func(fl validator.FieldLevel) bool {
+
+	if fl.Parent().Kind() != reflect.Struct {
+		panic(fmt.Sprintf("Invalid parent type %T: must be a struct", fl.Parent().Interface()))
+	}
+
 	_, _, ok := fl.GetStructFieldOK()
 
 	return ok
