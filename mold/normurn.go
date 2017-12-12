5e5dde6afd0e88fef82dde4fa5ed3ee29a7a94f8
Samuele Salines <samuele.saliness@gmail.com>
2017-12-12 17:14:47 +0100

    Update: Added invalid urn and bad field type errors

--- mold/normurn.go
+++ mold/normurn.go
@@ -9,21 +9,39 @@ import (
 	m "gopkg.in/go-playground/mold.v2"
 )
 
+// InvalidUrnError represents an error occuring during the urn normalization
+// in case it's an invalid urn
+type InvalidUrnError struct {
+	message string
+}
+
+// BadTypeError epresents an error occuring during the urn normalization
+// in case the input it's an invalid type
+type BadTypeError struct {
+	message string
+}
+
+func (e InvalidUrnError) Error() string { return e.message }
+
+func (e BadTypeError) Error() string { return e.message }
+
 func normurn(ctx context.Context, t *m.Transformer, v reflect.Value, param string) (e error) {
 	switch v.Interface().(type) {
 	case string:
 		val := v.String()
 		u, ok := urn.Parse(val)
 		if !ok {
-			e = &Error{
+			e = &InvalidUrnError{
 				message: fmt.Sprintf("Not a valid URN %s", val),
 			}
 		} else {
 			v.SetString(u.Normalize().String())
 		}
 	default:
-		e = &Error{
+		e = &BadTypeError{
 			message: fmt.Sprintf("Bad field type %T", v.Interface()),
 		}
 	}
+
+	return
 }
