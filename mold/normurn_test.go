ff83c1a03b90225e50f128cd9435667553b7c1cf
Samuele Salines <samuele.saliness@gmail.com>
2017-12-12 17:16:28 +0100

    Update: NormUrn testing

--- mold/normurn_test.go
+++ mold/normurn_test.go
@@ -1,21 +1,52 @@
 package mold
 
 import (
+	"context"
+	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type normurnTestCase struct {
 	val interface{} // input urn
-	// (todo) > complete
+	pos bool        // whether this is a positive test case or not - ie., should pass or not
+	res string      // resulting value
 }
 
-// (todo) > populate
 var normurnTests = []normurnTestCase{
-	{},
+
+	// Valid URN
+	{"urn:A:b", true, "urn:a:b"},
+	{"URN:A:b", true, "urn:a:b"},
+	{"URN:A:B", true, "urn:a:B"},
+	{"urn:foo:A123%C456", true, "urn:foo:A123%c456"},
+	{"URN:FOO:a123%2C456", true, "urn:foo:a123%2c456"},
+
+	// Not a valid URN
+	{"URN:a:", false, ""},
+
+	// Bad field type
+	{123, false, ""},
 }
 
 func TestNormUrn(t *testing.T) {
-	for i, test := range normurnTests {
-		// (todo) > complete
+	for _, test := range normurnTests {
+		input := test.val
+		err := Transformer.Field(context.Background(), &input, fmt.Sprintf("normurn=%s", test.val))
+		fmt.Println(err)
+		if test.pos {
+			require.Nil(t, err)
+			require.Equal(t, test.res, input)
+		} else {
+			switch err.(type) {
+			case *InvalidUrnError:
+				require.EqualError(t, err, fmt.Sprintf("Not a valid URN %s", test.val))
+			case *BadTypeError:
+				require.EqualError(t, err, fmt.Sprintf("Bad field type %T", test.val))
+			default:
+				require.Error(t, err)
+			}
+		}
 	}
 }
