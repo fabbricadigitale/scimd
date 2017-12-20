b7826b40f2643b115200759ff55311fac305bc0f
skryja <alessandro.priori90@gmail.com>
2017-12-20 11:41:36 +0100

    Update: Added validators to ListResponse struct

--- api/messages/list.go
+++ api/messages/list.go
@@ -4,13 +4,14 @@ import (
 	"github.com/fabbricadigitale/scimd/schemas/resource"
 )
 
+// ListResponseURI is the URI identifies the list of responses
 const ListResponseURI = "urn:ietf:params:scim:api:messages:2.0:ListResponse"
 
-// ListResponse ...
+// ListResponse represents a list of responses as per http://myfabbrica.fabbricadigitale.it
 type ListResponse struct {
-	Schemas      []string             `json:"schemas"`
-	TotalResults int                  `json:"totalResults"`
-	ItemsPerPage int                  `json:"itemsPerPage"`
-	StartIndex   int                  `json:"startIndex"`
-	Resources    []*resource.Resource `json:"Resources"`
+	Schemas      []string             `json:"schemas" validate:"eq=1,dive,eq=urn:ietf:params:scim:api:messages:2.0:ListResponse"`
+	TotalResults int                  `json:"totalResults" validate:"required"`
+	ItemsPerPage int                  `json:"itemsPerPage" validate:"required"`    // we set this always required even if the RFC 7644 specifies that it's required due to pagination
+	StartIndex   int                  `json:"startIndex" validate:"gt=0,required"` // we set this always required even if the RFC 7644 specifies that it's required due to pagination
+	Resources    []*resource.Resource `json:"Resources" validate:"required"`
 }
