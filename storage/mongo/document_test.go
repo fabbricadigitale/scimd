76f17a0a9d5ece4798f9865ed6d13e17c137410b
Samuele Salines <samuele.saliness@gmail.com>
2017-12-20 14:55:53 +0100

    Update: Testing escaping and unescaping of a key from a json file

--- storage/mongo/document_test.go
+++ storage/mongo/document_test.go
@@ -1,8 +1,12 @@
 package mongo
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -26,4 +30,21 @@ func TestDocumentKeyEscaping(t *testing.T) {
 
 	assert.Equal(t, "foo", d[invalidKey])
 
+	// Escape - Unescape key from json
+
+	data, err := ioutil.ReadFile("../testdata/keyescaping_document.json")
+	require.NotNil(t, data)
+	require.NoError(t, err)
+
+	r := make(document)
+	err = json.Unmarshal(data, &r)
+	require.NoError(t, err)
+
+	r.escapeKeys()
+	assert.Nil(t, r[invalidKey])
+	assert.Equal(t, "foo", r[keyEscape(invalidKey)])
+
+	r.unescapeKeys()
+
+	assert.Equal(t, "foo", r[invalidKey])
 }
