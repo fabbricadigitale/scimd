48afcef3ddc988d13bb2ea5dbb25dcf90b75b6bb
Leonardo Di Donato <leodidonato@gmail.com>
2017-12-21 18:00:25 +0100

    Fix: Pointers dereferencing and "never" attributes

--- api/attr/projection.go
+++ api/attr/projection.go
@@ -6,30 +6,55 @@ import (
 	"gopkg.in/fatih/set.v0"
 )
 
-// Projection computes the attribute paths to be ...
+// Projection computes a set of attribute paths
+//
+// The logic it enables is the one described within the RFC at sections:
+//
+// - https://tools.ietf.org/html/rfc7644#section-3.4.2.5
+//
+// - https://tools.ietf.org/html/rfc7644#section-3.4.3
+//
+// - https://tools.ietf.org/html/rfc7644#section-3.9
+//
+// In short the logic is (D - E - N) ∪ A where:
+//
+// I: { i | i explicitly included by the user }
+//
+// E: { e | e explicitly excluded by the user }
+//
+// N: { n | n.Returned == "never" }
+//
+// A: { a | a.Returned == "always" }
+//
+// D: I if I != ∅ || { d | d.Returned == "default" }
 func Projection(ctx *core.ResourceType, included []*Path, excluded []*Path) []*Path {
 	always := set.New()
 	for _, a := range Paths(ctx, withReturned(schemas.ReturnedAlways)) {
-		always.Add(a)
+		always.Add(*a)
+	}
+
+	never := set.New()
+	for _, n := range Paths(ctx, withReturned(schemas.ReturnedNever)) {
+		never.Add(*n)
 	}
 
 	defaults := set.New()
 	if len(included) > 0 {
 		for _, d := range included {
-			defaults.Add(d)
+			defaults.Add(*d)
 		}
 	} else {
 		for _, d := range Paths(ctx, withReturned(schemas.ReturnedDefault)) {
-			defaults.Add(d)
+			defaults.Add(*d)
 		}
 	}
 
 	exclusions := set.New()
 	for _, e := range excluded {
-		exclusions.Add(e)
+		exclusions.Add(*e)
 	}
 
-	ret := set.Union(set.Difference(defaults, exclusions), always)
+	ret := set.Union(set.Difference(defaults, exclusions, never), always)
 
 	return getPathSlice(ret)
 }
@@ -37,12 +62,12 @@ func Projection(ctx *core.ResourceType, included []*Path, excluded []*Path) []*P
 func getPathSlice(ret set.Interface) []*Path {
 	res := make([]*Path, 0)
 	for _, item := range ret.List() {
-		v, ok := item.(*Path)
+		v, ok := item.(Path)
 		if !ok {
 			continue
 		}
 
-		res = append(res, v)
+		res = append(res, &v)
 	}
 	return res
 }
