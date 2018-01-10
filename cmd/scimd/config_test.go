297b80b5f2c684adba27c917f72f0b58c4811cd5
alessandro <sandrone.lb@gmail.com>
2018-01-10 18:21:45 +0100

    Update: Testing repos have been created when calling config() func

--- cmd/scimd/config_test.go
+++ cmd/scimd/config_test.go
@@ -5,22 +5,29 @@ import (
 	"github.com/fabbricadigitale/scimd/schemas/core"
 	"testing"
 )
+// Note: testing phase 1 has static expected values.
+const (
+	//Number of JSON files within default/schemas directory
+	x = 3
+	//Number of JSON files within default/resources directory
+	y = 2
+)
 
 func TestConfig(t *testing.T) {
 
 	spc := config()
 	require.IsType(t, spc, &core.ServiceProviderConfig{})
 
+	schemaRepo := core.GetSchemaRepository()
+	require.Equal(t, x, len(schemaRepo.List()))
+
+	resTypeRepo := core.GetResourceTypeRepository()
+	require.Equal(t, y, len(resTypeRepo.List()))
+
 	// (todo)
-	// phase 1
-	// - x = # of JSON files within default/schemas directory
-	// - y = # of JSON files within default/resources directory
-	// - call config()
-	// - test it returns a ServiceProviderConfig instance
-	// - test a schema repository containing x schemas has been created
-	// - test a resource type repository containing y resource types has been created
 	// phase 2 - requires parametrization (path of service provider config JSON) of config function
 	// - test panics when wrong path
 	// - test panics with unmarshalling errors
 	// - test panics with validation errors
 }
+
