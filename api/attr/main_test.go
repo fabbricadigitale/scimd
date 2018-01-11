12e22416d19c4483fc5662712c27685f8a26e669
Leonardo Grasso <me@leonardograsso.com>
2018-01-11 15:46:08 +0100

    Update: Preload testdata for resource

--- api/attr/main_test.go
+++ api/attr/main_test.go
@@ -1,13 +1,17 @@
 package attr
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"testing"
 
 	"github.com/fabbricadigitale/scimd/schemas/core"
+	"github.com/fabbricadigitale/scimd/schemas/resource"
 )
 
 var resTypeRepo core.ResourceTypeRepository
 var schemaRepo core.SchemaRepository
+var userRes = resource.Resource{}
 
 func TestMain(m *testing.M) {
 	// Test setup
@@ -16,6 +20,15 @@ func TestMain(m *testing.M) {
 
 	schemaRepo = core.GetSchemaRepository()
 	schemaRepo.Add("../../internal/testdata/user_schema.json")
+	schemaRepo.Add("../../internal/testdata/enterprise_user_schema.json")
+
+	userData, err := ioutil.ReadFile("../../internal/testdata/enterprise_user_resource_1.json")
+	if err != nil {
+		panic(err)
+	}
+	if err = json.Unmarshal(userData, &userRes); err != nil {
+		panic(err)
+	}
 
 	// Test run
 	m.Run()
