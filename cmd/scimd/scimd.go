ef9a9ab44e6d5310e6d6f24a20cf40072582f028
Leonardo Di Donato <leodidonato@gmail.com>
2018-01-22 17:24:48 +0100

    Docs: Endpoint documentation

--- cmd/scimd/scimd.go
+++ cmd/scimd/scimd.go
@@ -39,37 +39,48 @@ func setup() *gin.Engine {
 		v2.Use(server.Authentication(authScheme.Type))
 	}
 
+	// Retrieve service provider config
 	v2.GET(svcpcfgEndpoint, getting)
+	// Retrieve supported resource types
 	v2.GET(restypeEndpoint, listing)
+	// Retrieve one or more supported schemas
 	v2.GET(schemasEndpoint, listing)
+	v2.GET(fmt.Sprintf("%s/:id", schemasEndpoint), getting)
 
+	// Bulk updates to one or more supported schemas
 	if spc.Bulk.Supported {
 		v2.POST(bulkEndpoint, bulking)
 	}
 
+	// Alias for operations against a resource mapped to an authenticated subject
 	v2.GET(selfEndpoint, selfing)
 	v2.POST(selfEndpoint, selfing)
 	v2.PUT(fmt.Sprintf("%s/:id", selfEndpoint), selfing)
 	v2.PATCH(fmt.Sprintf("%s/:id", selfEndpoint), selfing)
 	v2.DELETE(fmt.Sprintf("%s/:id", selfEndpoint), selfing)
+
+	// Search from system root for one or more resource types using POST
 	v2.POST(fmt.Sprintf("/%s", searchAction), getting)
 
 	for _, rt := range core.GetResourceTypeRepository().List() {
-		v2.GET(fmt.Sprintf("%s/%s", restypeEndpoint, rt.GetIdentifier()), getting)
+		// (todo) > verify whether RFC specifies endpoint to retrieve resource type by identifier, or not
+		// v2.GET(fmt.Sprintf("%s/%s", restypeEndpoint, rt.GetIdentifier()), getting)
+
+		// List all resources
 		v2.GET(rt.Endpoint, listing)
+
+		// Create new resource
 		v2.POST(rt.Endpoint, posting)
+
+		// Search within a resource endpoint for one or more resource types using POST
 		v2.POST(fmt.Sprintf("%s/%s", rt.Endpoint, searchAction), getting)
 
+		// Retrieve, add, modify, or delete resource
 		v2.GET(fmt.Sprintf("%s/:id", rt.Endpoint), getting)
 		v2.PUT(fmt.Sprintf("%s/:id", rt.Endpoint), putting)
 		v2.PATCH(fmt.Sprintf("%s/:id", rt.Endpoint), patching)
 		v2.DELETE(fmt.Sprintf("%s/:id", rt.Endpoint), deleting)
 	}
-
-	// (fixme) > schema IDs must be escaped
-	//	for _, sc := range core.GetSchemaRepository().List() {
-	//		v2.GET(fmt.Sprintf("%s/%s", schemasEndpoint, sc.GetIdentifier()), getting)
-	//	}
 
 	return router
 }
