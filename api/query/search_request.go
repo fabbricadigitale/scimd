cfd94299883feb2759b519465f3e8941d9f4d29b
Leonardo Grasso <me@leonardograsso.com>
2018-01-23 17:33:24 +0100

    New: Query API via SearchRequest message

---
+++ api/query/search_request.go
@@ -0,0 +1,13 @@
+package query
+
+
+import (
+	"github.com/fabbricadigitale/scimd/api/messages"
+	"github.com/fabbricadigitale/scimd/schemas/core"
+	"github.com/fabbricadigitale/scimd/storage"
+)
+
+func SearchRequest(s storage.Storer, resTypes []*core.ResourceType, search *messages.SearchRequest) (list *messages.ListResponse, err error) {
+	// (todo) Validate rew
+	return Resources(s, resTypes, &search.Search)
+}
