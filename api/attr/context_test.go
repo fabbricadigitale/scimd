70ccdcaf5f8f234472f4c6b867daef4d88550920
Samuele Salines <samuele.saliness@gmail.com>
2018-02-06 14:29:24 +0100

    New: Testing Contexts() method (refs #57)

--- api/attr/context_test.go
+++ api/attr/context_test.go
@@ -1,12 +1,196 @@
 package attr
 
 import (
+	"github.com/fabbricadigitale/scimd/schemas"
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/fabbricadigitale/scimd/schemas/core"
 	"github.com/fabbricadigitale/scimd/schemas/datatype"
 	"github.com/stretchr/testify/assert"
 )
+
+var withFxNil = map[string]bool{
+	"schemas": true,
+	"id": true,
+	"externalId": true,
+	"meta": true,
+	"meta.resourceType": true,
+	"meta.created": true,
+	"meta.lastModified": true,
+	"meta.location": true,
+	"meta.version": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:employeeNumber": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:costCenter": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:organization": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:division": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:department": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:manager": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:manager.value": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:manager.$ref": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:manager.displayName": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:userName": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:name": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:name.formatted": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:name.familyName": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:name.givenName": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:name.middleName": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:name.honorificPrefix": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:name.honorificSuffix": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:displayName": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:nickName": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:profileUrl": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:title": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:userType": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:preferredLanguage": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:locale": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:timezone": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:active": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:password": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:emails": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:emails.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:emails.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:emails.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:emails.primary": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:phoneNumbers": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:phoneNumbers.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:phoneNumbers.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:phoneNumbers.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:phoneNumbers.primary": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:ims": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:ims.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:ims.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:ims.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:ims.primary": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:photos": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:photos.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:photos.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:photos.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:photos.primary": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.formatted": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.streetAddress": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.locality": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.region": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.postalCode": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.country": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.primary": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:groups": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:groups.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:groups.$ref": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:groups.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:groups.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:entitlements": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:entitlements.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:entitlements.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:entitlements.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:entitlements.primary": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:roles": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:roles.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:roles.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:roles.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:roles.primary": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:x509Certificates": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:x509Certificates.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:x509Certificates.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:x509Certificates.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:x509Certificates.primary": true,
+}
+
+var withReturnedAlways = map[string]bool{
+	"schemas": true,
+	"id": true,
+	"meta.resourceType": true,
+}
+
+var withReturnedNever = map[string]bool{
+	"urn:ietf:params:scim:schemas:core:2.0:User:password": true,
+}
+
+var withReturnedDefault = map[string]bool{
+	"externalId": true,
+	"meta": true,
+	"meta.created": true,
+	"meta.lastModified": true,
+	"meta.location": true,
+	"meta.version": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:employeeNumber": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:costCenter": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:organization": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:division": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:department": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:manager": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:manager.value": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:manager.$ref": true,
+	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:manager.displayName": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:userName": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:name": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:name.formatted": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:name.familyName": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:name.givenName": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:name.middleName": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:name.honorificPrefix": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:name.honorificSuffix": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:displayName": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:nickName": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:profileUrl": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:title": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:userType": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:preferredLanguage": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:locale": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:timezone": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:active": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:emails": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:emails.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:emails.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:emails.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:emails.primary": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:phoneNumbers": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:phoneNumbers.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:phoneNumbers.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:phoneNumbers.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:phoneNumbers.primary": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:ims": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:ims.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:ims.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:ims.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:ims.primary": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:photos": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:photos.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:photos.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:photos.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:photos.primary": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.formatted": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.streetAddress": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.locality": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.region": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.postalCode": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.country": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:addresses.primary": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:groups": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:groups.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:groups.$ref": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:groups.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:groups.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:entitlements": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:entitlements.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:entitlements.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:entitlements.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:entitlements.primary": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:roles": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:roles.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:roles.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:roles.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:roles.primary": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:x509Certificates": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:x509Certificates.value": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:x509Certificates.display": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:x509Certificates.type": true,
+	"urn:ietf:params:scim:schemas:core:2.0:User:x509Certificates.primary": true,
+}
 
 func TestContext(t *testing.T) {
 	rt := resTypeRepo.Get("User")
@@ -265,3 +449,41 @@ func TestContextDelete(t *testing.T) {
 }
 
 // (todo) > Test contexts() with filter about attributes sub attributes characteristic
+
+func TestContexts(t *testing.T) {
+	rt := resTypeRepo.Get("User")
+
+	var fxNilAttrs =  make(map[string]bool)
+	fxnil := Contexts(rt, nil)	
+	for _, at := range fxnil {
+		fxNilAttrs[at.Path().String()] = true
+	}
+	require.Equal(t, withFxNil, fxNilAttrs)
+	
+	var raAttrs =  make(map[string]bool)
+	ra := Contexts(rt, withReturned(schemas.ReturnedAlways))
+	for _, at := range ra {
+		raAttrs[at.Path().String()] = true
+	}
+	require.Equal(t, withReturnedAlways, raAttrs)
+
+	var rnAttrs =  make(map[string]bool)
+	rn := Contexts(rt, withReturned(schemas.ReturnedNever))
+	for _, at := range rn {
+		rnAttrs[at.Path().String()] = true
+	}
+	require.Equal(t, withReturnedNever, rnAttrs)
+
+	var rdAttrs =  make(map[string]bool)
+	rd := Contexts(rt, withReturned(schemas.ReturnedDefault))
+	for _, at := range rd {
+		rdAttrs[at.Path().String()] = true
+	}
+	require.Equal(t, withReturnedDefault, rdAttrs)
+
+	// TODO > What to do when rt is empty? Panics in core/resource_type/#58 on the GetIdentifier() method 
+	//  
+	// rt := &core.ResourceType{}
+	// rd := Contexts(rt, withReturned(schemas.ReturnedDefault))
+
+}
