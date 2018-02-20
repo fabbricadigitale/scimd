461e44a7ff84d526105db960420f3a97f6603ada
Samuele Salines <samuele.saliness@gmail.com>
2018-02-20 14:40:54 +0100

    New: Testing pathExists validator

---
+++ validation/pathexists_test.go
@@ -0,0 +1,32 @@
+package validation
+
+import (
+	"testing"
+	"path/filepath"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPathExists(t *testing.T){
+	var err error
+	var absPath string
+
+	okPath := "../../scimd/internal/testdata/service_provider_config.json"
+	absPath, _ = filepath.Abs(okPath)
+	err = Validator.Var(absPath, "pathexists")
+	require.NoError(t, err)
+
+	wrongPath := "../../scimd/internal/testdata/service_provider.json"
+	absPath, _ = filepath.Abs(wrongPath)
+	err = Validator.Var(absPath, "pathexists")
+	require.Error(t, err)
+
+	emptyPath := ""
+	err = Validator.Var(emptyPath, "pathexists")
+	require.Error(t, err)
+
+	badType := 123123
+	require.PanicsWithValue(t, "Bad field type int", func(){
+		err = Validator.Var(badType, "pathexists")
+	})
+}
