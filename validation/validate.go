9687fe42573a3b2662899b98da4551399161c8dc
Leonardo Di Donato <leodidonato@gmail.com>
2018-02-21 11:46:12 +0100

    New: I18n for validator errors

--- validation/validate.go
+++ validation/validate.go
@@ -1,14 +1,35 @@
 package validation
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
 	validator "gopkg.in/go-playground/validator.v9"
+	en_translations "gopkg.in/go-playground/validator.v9/translations/en"
 )
 
 // Validator is a singleton instance providing validation functionalities
 var Validator *validator.Validate
 
+var (
+	uni             *ut.UniversalTranslator
+	translator      ut.Translator
+	translatorFound bool
+)
+
 func init() {
+	eng := en.New()
+	uni := ut.New(eng, eng)
+	translator, translatorFound = uni.GetTranslator("en")
+	if !translatorFound {
+		panic("translator not found")
+	}
+
 	Validator = validator.New()
+	en_translations.RegisterDefaultTranslations(Validator, translator)
+
 	Validator.RegisterValidation("startswith", startsWith)
 	Validator.RegisterValidation("nstartswith", notStartsWith)
 	Validator.RegisterValidation("endswith", endsWith)
@@ -21,4 +42,35 @@ func init() {
 	Validator.RegisterValidation("pathexists", pathExists)
 	Validator.RegisterValidation("isdir", isDirectory)
 	Validator.RegisterValidation("isfile", isFile)
+}
+
+// Errors translates and returns all the error messages by namespace
+//
+// Notice this means that when multiple errors exists only the last one will be selected.
+// Order between different namespaces is not guaranteed.
+func Errors(err error) string {
+	trans := make(validator.ValidationErrorsTranslations)
+	if err != nil {
+		// Translate all error at once by namespace
+		errs := err.(validator.ValidationErrors)
+		trans = errs.Translate(translator)
+	}
+
+	res := []string{}
+	for ns, te := range trans {
+		parts := []string{}
+		for i, p := range strings.Split(ns, ".") {
+			if i > 1 {
+				parts = append(parts, strings.ToLower(p))
+			} else if i > 0 {
+				parts = append(parts, p)
+			}
+		}
+
+		withStructName := strings.Join(parts, " ")
+		withoutFieldName := strings.TrimLeft(te, strings.Title(parts[len(parts)-1]))
+
+		res = append(res, fmt.Sprintf("%s%s.", withStructName, withoutFieldName))
+	}
+	return strings.Join(res, "\n")
 }
