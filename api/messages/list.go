8e3643487f9b57413032a8a575d77ffda557931d
Alessandro La Bella <sandrone.lb@gmail.com>
2018-02-22 14:21:40 +0100

    Update: Change list.resources type to slice of interface

--- api/messages/list.go
+++ api/messages/list.go
@@ -1,19 +1,15 @@
 package messages
 
-import (
-	"github.com/fabbricadigitale/scimd/schemas/resource"
-)
-
 // ListResponseURI is the URI identifies the list of responses
 const ListResponseURI = "urn:ietf:params:scim:api:messages:2.0:ListResponse"
 
 // ListResponse represents a list of responses as per http://myfabbrica.fabbricadigitale.it
 type ListResponse struct {
-	Schemas      []string             `json:"schemas" validate:"eq=1,dive,eq=urn:ietf:params:scim:api:messages:2.0:ListResponse"`
-	TotalResults int                  `json:"totalResults" validate:"required"`
-	ItemsPerPage int                  `json:"itemsPerPage" validate:"required"`    // we set this always required even if the RFC 7644 specifies that it's required due to pagination
-	StartIndex   int                  `json:"startIndex" validate:"gt=0,required"` // we set this always required even if the RFC 7644 specifies that it's required due to pagination
-	Resources    []*resource.Resource `json:"Resources" validate:"required"`
+	Schemas      []string      `json:"schemas" validate:"eq=1,dive,eq=urn:ietf:params:scim:api:messages:2.0:ListResponse"`
+	TotalResults int           `json:"totalResults" validate:"required"`
+	ItemsPerPage int           `json:"itemsPerPage" validate:"required"`    // we set this always required even if the RFC 7644 specifies that it's required due to pagination
+	StartIndex   int           `json:"startIndex" validate:"gt=0,required"` // we set this always required even if the RFC 7644 specifies that it's required due to pagination
+	Resources    []interface{} `json:"Resources" validate:"required"`
 }
 
 func NewListResponse() *ListResponse {
