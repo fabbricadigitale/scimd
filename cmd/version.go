e14050454132b716fec186405c17f92b078330bd
Leonardo Di Donato <leodidonato@gmail.com>
2018-02-23 18:02:44 +0100

    Update: Version CLI command completed

--- cmd/version.go
+++ cmd/version.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/fabbricadigitale/scimd/config"
 	"github.com/spf13/cobra"
 )
 
-var version string
+var (
+	version string
+	commit  string
+	branch  string
+	summary string
+	date    string
+)
 
 func init() {
 	scimd.AddCommand(versionCmd)
@@ -16,9 +23,19 @@ func init() {
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of scimd",
-	Long:  `Shows the version number of scimd.`,
+	Long: `This command shows the version number of scimd.
+	
+It also can print detailed information about the build process of the current scimd.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Fprintf(os.Stdout, "Version %s.\n", version)
+
+		if config.Values.Debug {
+			fmt.Println()
+			fmt.Fprintf(os.Stdout, "Commit\t\t%s.\n", commit)
+			fmt.Fprintf(os.Stdout, "Branch\t\t%s.\n", branch)
+			fmt.Fprintf(os.Stdout, "Summary\t\t%s.\n", summary)
+			fmt.Fprintf(os.Stdout, "Build date\t%s.\n", date)
+		}
 	},
 	DisableAutoGenTag: true,
 }
