a6697f45e68fedd8912f8f012d43b95946c235d9
Leonardo Grasso <me@leonardograsso.com>
2018-02-25 01:41:02 +0100

    Update: Validation steps for creation API (refs #7)
    cc @alelb @leodido
    This commit implements validation steps for creation as described by RFC (with the only expection of "schemas").
    Some attributes are asserted by client, other by service provider, thus we cannot simply perform a centralized validation on the entity (eg. using struct tags), but we need to handle each situation in a proper way.
    For example, "readOnly" attrs can be asserted only by the service provider so it makes no sense to check for these attrs if a required value has been provided by the client while creating a resource.

--- api/create/resource.go
+++ api/create/resource.go
@@ -16,26 +16,37 @@ import (
 
 // Resource creates a new res of type resType and stores it into s.
 //
-// This func expects that res was populated according to the given resType.
+// This func expects that res was populated according to the given resType
+// (ie. type validation is not performed here).
+//
 // Commons' attributes, if present, will be ignored and overwritten
 // (with the only exception of ExternalID that if populated will be used).
-// Attributes whose mutability is "readOnly" will be ignored and removed.
-func Resource(s storage.Storer, resType *core.ResourceType, res *resource.Resource) (ret core.ResourceTyper, err error) {
+//
+// Attributes whose mutability is "readOnly" will be ignored and removed,
+// other attributes can be written while creation.
+//
+// If a required attributes is missing an error is returned, with exception of
+// "readOnly" ones whose are just ignored.
+//
+// Finally, this func expects that uniqueness conflict are handled by storage.Storer
+func Resource(s storage.Storer, resType *core.ResourceType, res *resource.Resource) (core.ResourceTyper, error) {
 
-	// Make a new UUID
-	ID, err := uuid.NewV4()
-	if err != nil {
-		return
-	}
-
-	// Setup commons
-	res.ID = ID.String()
-
+	// (todo) Could client assert "schemas"?
+	// If so, we must validate asserted schemas against available schema on current resType.
+	// Currently, not yet supported.
+	// Note also that resType cannot be asserted by client.
 	res.Schemas = make([]string, len(resType.SchemaExtensions)+1)
 	res.Schemas[0] = resType.Schema
 	for i, ext := range resType.SchemaExtensions {
 		res.Schemas[i+1] = ext.Schema
 	}
+
+	// ID, ResourceType and other stuff that can be only asserted by the service provider
+	ID, err := uuid.NewV4()
+	if err != nil {
+		return nil, err
+	}
+	res.ID = ID.String()
 
 	now := time.Now()
 	res.Meta = core.Meta{
@@ -45,25 +56,27 @@ func Resource(s storage.Storer, resType *core.ResourceType, res *resource.Resour
 		Version:      version.GenerateVersion(true, ID.String(), now.String()),
 	}
 
+	// Since the ResourceType was set, we can check required
+	if err := attr.CheckRequired(res); err != nil {
+		return nil, err
+	}
+
 	// Attributes whose mutability is "readOnly" SHALL be ignored
 	ro, err := attr.Paths(resType, func(attribute *core.Attribute) bool {
 		return attribute.Mutability == schemas.MutabilityReadOnly
 	})
 	if err != nil {
-		ret = nil
-		return
+		return nil, err
 	}
 
 	for _, p := range ro {
 		p.Context(resType).Delete(res)
 	}
 
-	err = s.Create(res)
-	if err != nil {
-		ret = nil
-	} else {
-		ret, err = query.Resource(s.(storage.Storer), resType, res.ID, &api.Attributes{})
+	// Assume s.Create will return an error in case of uniquiness conflict
+	if err := s.Create(res); err != nil {
+		return nil, err
 	}
 
-	return
+	return query.Resource(s.(storage.Storer), resType, res.ID, &api.Attributes{})
 }
