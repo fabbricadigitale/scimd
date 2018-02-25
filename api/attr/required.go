bafbf3ec001bcbf3272062d53cf628691ec0d6e9
Leonardo Grasso <me@leonardograsso.com>
2018-02-25 01:22:33 +0100

    New: API to ensure required attrs

---
+++ api/attr/required.go
@@ -0,0 +1,51 @@
+package attr
+
+import (
+	"github.com/fabbricadigitale/scimd/api"
+	"github.com/fabbricadigitale/scimd/api/messages"
+	"github.com/fabbricadigitale/scimd/schemas"
+	"github.com/fabbricadigitale/scimd/schemas/core"
+	"github.com/fabbricadigitale/scimd/schemas/datatype"
+	"github.com/fabbricadigitale/scimd/schemas/resource"
+)
+
+// CheckRequired returns an error if at least one required attribute of res was not asserted by the client
+func CheckRequired(res *resource.Resource) (err error) {
+
+	// With the only execption of "schemas", commons required attrs will be not checked because
+	// all of them have "readOnly" mutability which attrs cannot be asserted by the client
+	// and must be ignored.
+
+	if len(res.Schemas) == 0 {
+		return messages.NewError(&api.MissingRequiredPropertyError{
+			Path: "schemas",
+		})
+	}
+
+	attrs, err := Paths(res.ResourceType(), func(attribute *core.Attribute) bool {
+		return (attribute.Required == true && attribute.Mutability != schemas.MutabilityReadOnly)
+	})
+
+	if err != nil {
+		return
+	}
+
+	for _, a := range attrs {
+
+		ctx := a.Context(res.ResourceType())
+
+		if ctx.Schema == nil {
+			continue
+		}
+
+		v := ctx.Get(res)
+
+		if datatype.IsNull(v) {
+			err = messages.NewError(&api.MissingRequiredPropertyError{
+				Path: a.String(),
+			})
+		}
+
+	}
+	return
+}
