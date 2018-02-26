f8673fac494273acc0bd08bafba5c17cf78bebcf
Alessandro La Bella <sandrone.lb@gmail.com>
2018-02-26 10:48:10 +0100

    Update: Validation for replacing API (refs #7)

--- api/update/resource.go
+++ api/update/resource.go
@@ -4,7 +4,9 @@ import (
 	"time"
 
 	"github.com/fabbricadigitale/scimd/api"
+	"github.com/fabbricadigitale/scimd/api/attr"
 	"github.com/fabbricadigitale/scimd/api/query"
+	"github.com/fabbricadigitale/scimd/schemas"
 	"github.com/fabbricadigitale/scimd/schemas/core"
 	"github.com/fabbricadigitale/scimd/schemas/resource"
 	"github.com/fabbricadigitale/scimd/storage"
@@ -27,6 +29,23 @@ func Resource(s storage.Storer, resType *core.ResourceType, id string, res *reso
 	now := time.Now()
 	res.Meta.LastModified = &now
 	res.Meta.Version = version.GenerateVersion(true, res.ID, now.String())
+
+	// Since the ResourceType was set, we can check required
+	if err := attr.CheckRequired(res); err != nil {
+		return nil, err
+	}
+
+	// Attributes whose mutability is "readOnly" SHALL be ignored
+	ro, err := attr.Paths(resType, func(attribute *core.Attribute) bool {
+		return attribute.Mutability == schemas.MutabilityReadOnly
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, p := range ro {
+		p.Context(resType).Delete(res)
+	}
 
 	err = s.Update(res, id, "")
 	if err != nil {
