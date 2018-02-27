68ccee3c63c6f1769bfead3d0002c1caa8791755
Alessandro La Bella <sandrone.lb@gmail.com>
2018-02-27 15:42:15 +0100

    Update: Utility to get unique attributes of all the schemas

---
+++ schemas/attributes/unique.go
@@ -0,0 +1,34 @@
+package attributes
+
+import (
+	"github.com/fabbricadigitale/scimd/api/attr"
+	"github.com/fabbricadigitale/scimd/schemas"
+	"github.com/fabbricadigitale/scimd/schemas/core"
+)
+
+// Unique is ...
+// (todo) > add support to multi key indexes
+func Unique() ([][]string, error) {
+
+	list := core.GetResourceTypeRepository().List()
+
+	var uniqueAttrs [][]string
+	uniqueAttrs = make([][]string, 0)
+
+	for _, resType := range list {
+
+		ro, err := attr.Paths(&resType, func(attribute *core.Attribute) bool {
+			return attribute.Uniqueness == schemas.UniquenessServer || attribute.Uniqueness == schemas.UniquenessGlobal
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		for _, p := range ro {
+			uniqueAttrs = append(uniqueAttrs, []string{p.String()})
+		}
+
+	}
+
+	return uniqueAttrs, nil
+}
